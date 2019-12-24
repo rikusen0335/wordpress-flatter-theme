84ce993f7ada50450ea52f00584b572eaee19743
rikusen0335 <rikusen0335@gmail.com>
2019-12-25 05:33:14 +0900

    @mdi/font toka @mdi/js wo include dekinaiiiiiiiiiiiii

--- resources/assets/scripts/main.js
+++ resources/assets/scripts/main.js
@@ -10,6 +10,9 @@ import common from './routes/common';
 import home from './routes/home';
 import aboutUs from './routes/about';
 
+// import MaterialDesignIcons
+//import '@mdi/js';
+
 /** Populate Router instance with DOM routes */
 const routes = new Router({
   // All pages
--- resources/views/partials/entry-meta.blade.php
+++ resources/views/partials/entry-meta.blade.php
@@ -1,5 +1,5 @@
 <time class="updated mr-3" datetime="{{ get_post_time('c', true) }}">公開日: {{ get_the_date() }}</time>
-<time datetime="{{ get_post_time('c', true) }}">更新日: {{ get_the_modified_date() }}</time>
+<time datetime="{{ get_post_time('c', true) }}"><span class="mdi mdi-update"></span>{{ get_the_modified_date() }}</time>
 <p class="byline author vcard">
   {{ __('By', 'sage') }} <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">
     {{ get_the_author() }}
