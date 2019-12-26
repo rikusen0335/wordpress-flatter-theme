8909b87e456881014a49346546fe2186c645007a
rikusen0335 <rikusen0335@gmail.com>
2019-12-26 22:54:24 +0900

    ADD commented @include disqus, FIX card structure

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -16,5 +16,6 @@
       {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
     </footer>
     {{-- @php comments_template('/partials/comments.blade.php') @endphp --}}
+    {{-- @include ('partials/disqus') --}}
   </article>
 </div>
--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -1,23 +1,29 @@
 <div class="col-md-4 col-sm-6 col-xs-120 mb-3">
   <article id="article-card" class="card" data-my-attribute="{{ get_permalink() }}" @php post_class() @endphp>
-    <img src="https://via.placeholder.com/500" class="card-img-top" alt="...">
+    @if (has_post_thumbnail())
+      {{ the_post_thumbnail('full', ['class' => 'card-img-top']) }}
+    @else
+      <img src="https://via.placeholder.com/500" class="card-img-top" alt="No image">
+    @endif
     <div class="card-body">
       <header>
+        <div class="mb-2">
+          @if (has_tag())
+            @foreach (get_the_tags() as $tag)
+              <!-- tag icon? -->
+              <span class="badge badge-dark">{{ $tag -> name }}</span>
+            @endforeach
+          @endif
+        </div>
         <h5 class="card-title mb-1">{!! get_the_title() !!}</h5>
         <div class="mb-3">
-        @if (has_tag())
-          @foreach (get_the_tags() as $tag)
-            <!-- tag icon? -->
-            <span class="badge badge-dark">{{ $tag -> name }}</span>
-          @endforeach
-        @endif
 
-        <!-- category -->
-      </div>
+          <!-- category -->
+        </div>
 
-      <div class="mb-3">
-        @include('partials/entry-meta')
-      </div>
+        <div class="mb-3">
+          @include('partials/entry-meta')
+        </div>
 
       </header>
       <div class="card-text">
