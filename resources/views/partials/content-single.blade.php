4a348e94705a5da3a4a1b36c4426fc077ec337a7
rikusen0335 <rikusen0335@gmail.com>
2019-12-27 00:13:12 +0900

    ADD categories, tag display on single content, FIX styles

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -1,7 +1,7 @@
-<header class="jumbotron jumbotron-fluid entry-thumbnail entry-header" style="background-image: url({{ the_post_thumbnail_url() }});">
-  <div class="bg-inner">
-    <div class="container header-meta">
-      <h1 class="display-5 text-white">{!! get_the_title() !!}</h1>
+<header class="jumbotron jumbotron-fluid entry-thumbnail entry-header" style="background-image: url({{ get_the_post_thumbnail_url() }});">
+  <div class="entry-header-inner">
+    <div class="container position-relative">
+      <h1 class="display-5 text-white">{{ get_the_title() }}</h1>
       <span class="text-light">@include('partials/entry-meta')</span>
       <span>@include('partials/end-meta')</span>
     </div>
@@ -9,6 +9,21 @@
 </header>
 <div class="container">
   <article @php post_class() @endphp>
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="#">Home</a></li>
+        <li class="breadcrumb-item"><a href="#">Library</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Data</li>
+      </ol>
+      <div class="mb-5">
+        @if (has_tag())
+          @foreach (get_the_tags() as $tag)
+            <!-- tag icon? -->
+            <span class="badge badge-dark">{{ $tag -> name }}</span>
+          @endforeach
+        @endif
+      </div>
+    </nav>
     <div class="entry-content">
       @php the_content() @endphp
     </div>
